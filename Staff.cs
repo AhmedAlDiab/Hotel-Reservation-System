7b4e5c43aa2e5a4b3be936711229b9ffed5fa4f5
Ahmed Abdulkarim <ahmedaldiabdev@gmail.com>
2025-04-27 11:50:48 +0300

    Made the Staff Class (Waiting For Room Classes)

---
+++ Staff.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Reservation_System
+{
+    public class Staff : User
+    {
+        public void AddStanderdRoom()
+        {                
+            // Implementation for adding a standard room
+        }
+        public void AddDeluxeRoom()
+        {
+            // Implementation for adding a deluxe room
+        }
+        public void RemoveRoom()
+        {
+            // Implementation for removing a room
+        }
+        public Staff() { }
+        public Staff(int userID, string fullname, string phoneNumber, string email, string username, string password) : base(userID, fullname, phoneNumber, email, username, password){}
+    }
+}
