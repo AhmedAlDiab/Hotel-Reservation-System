31673d371f852ad372934f2513808373d6159349
Omar Abdelhak <134602966+Omar-Abdelhak@users.noreply.github.com>
2025-04-28 14:17:14 +0300

    Reservation class

---
+++ Reservation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+namespace Hotel_Reservation_System
+{
+    public class Reservation
+    {
+        private int reservationID;
+        private Customer customer;
+        private Room room;
+        private DateTime checkInDate;
+        private DateTime checkOutDate;
+        private double totalCost;
+        private EReservationStatus reservationStatus;
+
+        // Properties to access Data members
+        public int ReservationID
+        {
+            get { return reservationID }
+            set
+            {
+                if (value > 0)
+                    reservationID = value;
+                else
+                    reservationID = 0;
+            }
+        }
+
+        public Customer PCustomer
+        {
+            get { return customer}
+            set
+            {
+                customer.UserID
+            }
+        }
+
+    }
+}
