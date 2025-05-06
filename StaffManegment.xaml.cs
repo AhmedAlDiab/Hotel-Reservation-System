c5c59177aa72669e11e995ae927d06a63106e89c
Moriarty <aa71071011@gmail.com>
2025-05-07 00:02:47 +0300

    i edit pages and added new pages (user control) staff manegment and staff

---
+++ StaffManegment.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hotel_Reservation_System
+{
+    /// <summary>
+    /// Interaction logic for StaffManegment.xaml
+    /// </summary>
+    public partial class StaffManegment : UserControl
+    {
+        public ObservableCollection<Staff> GetStaff { get; set; }
+        public StaffManegment()
+        {
+            InitializeComponent();
+            GetStaff = new ObservableCollection<Staff>();
+            this.DataContext = this;
+        }
+
+        private void Remove(object sender, RoutedEventArgs e)
+        {
+            if (staffmaneg.SelectedItem is Staff selectStaff)
+                GetStaff.Remove(selectStaff);
+        }
+
+        private void add(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                Staff newstaff = new Staff { Fullname = "", PhoneNumber = "", Email = "", Username = "", Password = "" };
+                GetStaff.Add(newstaff);
+                staffmaneg.SelectedItem = newstaff;
+                staffmaneg.ScrollIntoView(newstaff);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+    }
+}
