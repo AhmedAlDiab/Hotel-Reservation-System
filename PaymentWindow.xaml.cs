651f57dad012c674109d07d84690ebc7a438dad6
AAK <oc899412634@gmail.com>
2025-05-16 17:32:10 +0300

    ComboBox Update

--- PaymentWindow.xaml.cs
+++ PaymentWindow.xaml.cs
@@ -23,5 +23,23 @@ namespace Hotel_Reservation_System
         {
             InitializeComponent();
         }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            int startYear = 2025;
+            int endYear = 2035;
+
+            for (int year = startYear; year <= endYear; year++)
+            {
+                for (int month = 1; month <= 12; month++)
+                {
+                    if (year == 2025 && month < 6)
+                        continue; // Skip months before June 2025
+
+                    string formattedMonth = month.ToString("D2"); // e.g., 01, 02, ..., 12
+                    string formattedYear = year.ToString().Substring(2); // e.g., 25, 26, ..., 50
+                    ExpireDate.Items.Add($"{formattedMonth}/{formattedYear}");
+                }
+            }
+        }
     }
 }
