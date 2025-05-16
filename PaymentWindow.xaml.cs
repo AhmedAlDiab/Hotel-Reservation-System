2291ed0c43d0d91a382fcdeb87c5ea156e57dffc
AAK <oc899412634@gmail.com>
2025-05-16 19:23:12 +0300

    Vlidation of Payment Page Update

--- PaymentWindow.xaml.cs
+++ PaymentWindow.xaml.cs
@@ -24,6 +24,87 @@ namespace Hotel_Reservation_System
             InitializeComponent();
         }
 
+        //--------------------------------------------------
+        // ------------------ VALIDATION -------------------
+        //--------------------------------------------------
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            string cardNumber = CreditCardNumber.Text.Trim();
+            string cvv = Cvv.Text.Trim();
+            string selectedCardType = (CreditCardType.SelectedItem as ComboBoxItem)?.Content.ToString();
+            string selectedExpireDate = ExpireDate.SelectedItem?.ToString();
+
+            // Checker
+            bool isValid = true;
+
+            //Submission Error If One Or More Field Has No Value
+            if (string.IsNullOrEmpty(cardNumber) || string.IsNullOrEmpty(cvv) || string.IsNullOrEmpty(selectedCardType) || string.IsNullOrEmpty(selectedExpireDate))
+            {
+                MessageBox.Show("Please Fill all Required Fields Correctly.", "Submission Error");
+                return;
+            }
+
+            //After Checking That All fields Has Value Then:
+
+
+            //---- 1st Check ----
+            // Credit Card Number: Must be 16 digits
+            if (cardNumber.Length != 16)
+            {
+                MessageBox.Show("Credit Card Number Must Be Exactly 16 digits.", "Input Error");
+                isValid = false;
+            }
+
+            //---- 2nd Check ----
+            //Credit Card Number: Must contain digits only
+            else if (!cardNumber.All(char.IsDigit))
+            {
+                MessageBox.Show("Credit Card Number Must Contain Digits Only (No Letters or Symbols).", "Input Error");
+                isValid = false;
+            }
+
+            //---- 3rd Check ----
+            // CVV: Must be 3 digits 
+            else if (cvv.Length != 3)
+            {
+                MessageBox.Show("CVV Must Be Exactly 3 Digits.", "Input Error");
+                isValid = false;
+            }
+
+            //---- 4th Check ----
+            // CVV: Must be digits only 
+            else if (!cvv.All(char.IsDigit))
+            {
+                MessageBox.Show("CVV Must Contain Digits Only (No Letters or Symbols).", "Input Error");
+                isValid = false;
+            }
+
+            //---- 5th Check ----
+            // Card Type: Must be Choosen 
+            else if (string.IsNullOrEmpty(selectedCardType))
+            {
+                MessageBox.Show("You Must Choose A Credit Card Type.", "Input Error");
+                isValid = false;
+            }
+
+            //---- 6th Check ----
+            // Expire Date: Must be Choosen 
+            else if (string.IsNullOrEmpty(selectedExpireDate))
+            {
+                MessageBox.Show("You Must Choose An Expiry Date.", "Input Error");
+                isValid = false;
+            }
+
+
+            //Finally If All 6 Check Points ----> True Then: submitted successfully
+            if (isValid)
+            {
+                MessageBox.Show("Payment Submitted Successfully!", "Successful Process");
+            }
+        }
+
+
+
         //ComboBox of ExpireDate Display Options From 06/25 To 06/35
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
