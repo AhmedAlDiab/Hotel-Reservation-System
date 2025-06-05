07240873c3b852c42f07de25e8ac4b24ee4b634c
Moriarty <aa71071011@gmail.com>
2025-06-05 13:53:34 +0300

    adding validation in user control ADDSTAFF

--- ADDSTAFF.xaml.cs
+++ ADDSTAFF.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Mysqlx.Crud;
+using Org.BouncyCastle.Asn1.Sec;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,6 +28,53 @@ namespace Hotel_Reservation_System
         {
             InitializeComponent();
         }
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new MailAddress(email);
+                // check if the domain contains a dot
+                string domainPart = addr.Host;
+                return domainPart.Contains('.') && addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private bool isvalidName(string name)
+        {
+            bool valid = true;
+            for (int i = 0; i < name.Length; i++)
+            {
+                if ((name[i] >= 'A' && name[i] <= 'Z') || (name[i] >= 'a' && name[i] <= 'z'))
+                {
+                    valid = true;
+                }
+                else
+                {
+                    valid= false;
+                    break;
+                }
+            }
+            return valid;
+        }
+        private bool isvalidNumber(string num)
+        {
+            bool valid = true;
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] >= '0' && num[i] <= '9')
+                {
+                    valid = true;
+                }
+                else
+                {
+                    valid = false; break;
+                }
+            }
+            return valid;
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -38,12 +87,27 @@ namespace Hotel_Reservation_System
                 string Pass=txtPassword.Text;
                 bool ISAD=chadmin.IsChecked==true;
                 Staff newstaff = new Staff();
-                newstaff.Fullname = FN;
-                newstaff.PhoneNumber = PN;
-                newstaff.Email = EM;
-                newstaff.IsAdmin = ISAD;
-                newstaff.Email = EM;
+                if (isvalidName(FN) == true)
+                    newstaff.Fullname = FN;
+                else
+                    MessageBox.Show("you must enter charcter only");
+                if(isvalidNumber(PN) == true)
+                    newstaff.PhoneNumber = PN;
+                else
+                    MessageBox.Show("you must enter Numbers only");
+
+                if (isvalidName(EM) == true)
+                    newstaff.Email = EM;
+                else
+                    MessageBox.Show("Value is not valid!");
+                    newstaff.IsAdmin = ISAD;
+               
                 newstaff.Password = Pass;
+
+                if (isvalidName(UN) == true)
+                    newstaff.Username = UN;
+                else
+                    MessageBox.Show("you must enter charcter only");
 
                 StaffManegment.staffs.Add(newstaff);
                 DataBase.AddUser(newstaff.Fullname, newstaff.PhoneNumber, newstaff.Email, newstaff.Username, newstaff.Password, newstaff.IsAdmin, DataBase.connectionString);
